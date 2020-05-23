e28b572895f3b6fd61c868e03d73d290be99fdfb
svetlanka-woman <svetlanka.woman@gmail.com>
2020-05-23 20:49:43 +0300

    functional slider

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,3 +1,39 @@
 $(function() {
+
+   //Drop down main menu
+
+   $('.toggle-submenu')
+      .on('mouseover', function () {
+         $(this).children('ul').show(200);
+      })
+      .on('mouseleave', function () {
+         $(this).children('ul').hide(100);
+      })
    
+   //main slider
+   
+   $('#main-slider .left-control-slider').hide(1);
+   
+   $('.right-control-slider').on('click', function () {
+      $('.item li.active-slide').toggleClass('active-slide').next().toggleClass('active-slide');
+      $('.item img.active-slide').toggleClass('active-slide').next().toggleClass('active-slide');
+      if ($('.item li.active-slide').attr('id')=="last") {
+         $(this).fadeOut(1);
+      }
+      if ($('.item li.active-slide').attr('id')!="first") {
+         $('.left-control-slider').fadeIn(300);
+      }
+   });
+   $('.left-control-slider').on('click', function () {
+         $('.item li.active-slide').toggleClass('active-slide').prev().toggleClass('active-slide');
+         $('.item img.active-slide').toggleClass('active-slide').prev().toggleClass('active-slide');
+         if ($('.item li.active-slide').attr('id')=="first") {
+            $(this).fadeOut(1);
+         }
+         if ($('.item li.active-slide').attr('id')!="last") {
+            $('.right-control-slider').fadeIn(300);
+         }
+      });
+
+
 });
