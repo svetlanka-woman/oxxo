a8e839065f7490f6c01055a6bf802d50b74fe829
svetlanka-woman <svetlanka.woman@gmail.com>
2020-06-18 15:20:21 +0300

    Adaptive layout of the header and functional of the drop-down submenu

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,39 +1,76 @@
 $(function() {
 
-   //Drop down main menu
+   $('.menu li:not(li.toggle-submenu)').on('click',function () {
+      if (window.innerWidth > 768) {
+         $('.submenu-1, .submenu-2').hide(1);
+      } else {
+         $('.menu .left-menu, .menu .right-menu').animate({'left':'-250px'},500);
+         $('.menu .btn-close').animate({'left':'-32px'},500);
+      }
+   });
+
+   //Drop down submenu
 
    $('.toggle-submenu')
       .on('mouseover', function () {
-         $(this).children('ul').show(200);
+         if (window.innerWidth > 768) {
+            $(this).children('ul').show(200);
+         }
       })
       .on('mouseleave', function () {
-         $(this).children('ul').hide(100);
-      })
+         if (window.innerWidth > 768) {
+            $(this).children('ul').hide(100);
+         }
+      });
+
+   $('.right-menu > li.toggle-submenu > a')
+      .on('click', function () {
+         if (window.innerWidth < 769) {
+            $(this).next().slideToggle(500);
+         }
+      });
+      $('.submenu-1 > li.toggle-submenu > a')
+      .on('click', function () {
+         if (window.innerWidth < 769) {
+            $(this).next().slideToggle(500);
+         }
+      });
    
+   //Toggle maim menu for screen < 769px
+
+   $('.menu .bar-toggle').on('click', function () {
+      $('.menu .right-menu').css('top',$('.menu .left-menu').innerHeight());
+      $('.menu .btn-close').animate({'left':'218px'},500);
+      $('.menu .left-menu, .menu .right-menu').animate({'left':'0px'},500);
+   });
+
+   $('.menu .btn-close').on('click', function () {
+      $('.menu .left-menu, .menu .right-menu').animate({'left':'-250px'},500);
+      $(this).animate({'left':'-32px'},500);
+   });
+
    //main slider
-   
-   $('#main-slider .left-control-slider').hide(1);
-   
+
    $('.right-control-slider').on('click', function () {
       $('.item li.active-slide').toggleClass('active-slide').next().toggleClass('active-slide');
       $('.item img.active-slide').toggleClass('active-slide').next().toggleClass('active-slide');
-      if ($('.item li.active-slide').attr('id')=="last") {
+      if ($('.item li.active-slide').attr('id') == "last") {
          $(this).fadeOut(1);
       }
-      if ($('.item li.active-slide').attr('id')!="first") {
+      if ($('.item li.active-slide').attr('id') != "first") {
          $('.left-control-slider').fadeIn(300);
       }
    });
    $('.left-control-slider').on('click', function () {
-         $('.item li.active-slide').toggleClass('active-slide').prev().toggleClass('active-slide');
-         $('.item img.active-slide').toggleClass('active-slide').prev().toggleClass('active-slide');
-         if ($('.item li.active-slide').attr('id')=="first") {
-            $(this).fadeOut(1);
-         }
-         if ($('.item li.active-slide').attr('id')!="last") {
-            $('.right-control-slider').fadeIn(300);
-         }
-      });
+      $('.item li.active-slide').toggleClass('active-slide').prev().toggleClass('active-slide');
+      $('.item img.active-slide').toggleClass('active-slide').prev().toggleClass('active-slide');
+      if ($('.item li.active-slide').attr('id') == "first") {
+         $(this).fadeOut(1);
+      }
+      if ($('.item li.active-slide').attr('id') != "last") {
+         $('.right-control-slider').fadeIn(300);
+      }
+   });
 
 
 });
